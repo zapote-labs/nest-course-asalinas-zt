f39dfd77d491a1fc529023cc5512cd0c91c1962d
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-30 17:42:33 -0500

    feat(Tasks): Added get all tasks
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private tasksService: TasksService){}
 
+	@Get()
+	getAllTasks(): Task[] {
+		return this.tasksService.getAllTasks();
+	}
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+import { Task } from './task.model';
+
 @Injectable()
 export class TasksService {
+	private tasks: Task[] = [];
+
+	public getAllTasks(): Task[] {
+		return this.tasks;
+	}
 }
