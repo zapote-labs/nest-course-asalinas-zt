83366c9e0feb45a5855db8398808e81275161342
asalinas-zt <71787259+asalinas-zt@users.noreply.github.com>
2020-10-03 16:02:52 -0500

    feat: creating a task (#1)
    * build: Generated new project with cli
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    * feat(Tasks): Created Task model
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    * feat(Tasks): Created Task DTO Class
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    * feat(Tasks): Created Tasks Module
    
    Controller and Service
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    * feat(Tasks): Added get all tasks
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    * feat(Tasks): Added create task
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    * feat(Tasks: Added get task by id
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>
    
    Co-authored-by: Samuel Zapote Granjeno <samuel@zapote.org>

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { v4 as uuid } from 'uuid';
+
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+	private tasks: Task[] = [];
 
-    getAllTasks(): Task[] {
-        return this.tasks;
-    }
+	public getAllTasks(): Task[] {
+		return this.tasks;
+	}
+
+	public getTaskById(id: string): Task {
+		return this.tasks.find(task => task.id === id);
+	}
+
+	public createTask(createTaskDto: CreateTaskDto): Task {
+		const { title, description } = createTaskDto;
+		const task: Task = {
+			id: uuid(),
+			title,
+			description,
+			status: TaskStatus.OPEN,
+		};
+
+		this.tasks.push(task);
+		return task;
+	}
 }
