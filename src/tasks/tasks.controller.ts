90840bd5fcb276778ccbf01b74590d69acf9541a
asalinas-zt <adrianasalinas@zapote.org>
2020-10-03 19:50:02 -0500

    feat(Tasks): Added get tasks with filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private tasksService: TasksService){}
 
 	@Get()
-	getAllTasks(): Task[] {
-		return this.tasksService.getAllTasks();
+	getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if(Object.keys(filterDto).length){
+      return this.tasksService.getTasksWithFilter(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
 	}
 
 	@Get('/:id')
@@ -33,7 +38,6 @@ export class TasksController {
     @Param('id') id: string, 
     @Body('status') status: TaskStatus,
   ): Task {
-    console.log('Holaa');
     return this.tasksService.updateTaskStatus(id, status);
   }
 
