a900a414ba5cc847b1717459d7738c2118759201
Samuel Zapote <samuel@zapote.org>
2020-10-08 20:11:24 -0500

    feat: Tasks Part Two (#3)
    * feat(Tasks): Added delete task by id
    
    * feat(Tasks): Added update task status
    
    * feat(Tasks): Created Task Filter DTO
    
    * feat(Tasks): Added get tasks with filter
    
    * build(npm): Installed class-transformer and class-validator
    
    * feat(Tasks): Added validation pipe to create a task
    
    * feat(Tasks): Added error handling for nonexistent tasks
    
    * feat(Tasks): Added error handling for deleting nonexistent tasks
    
    Co-authored-by: asalinas-zt <adrianasalinas@zapote.org>

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private tasksService: TasksService){}
 
 	@Get()
-	getAllTasks(): Task[] {
-		return this.tasksService.getAllTasks();
+	getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+    if(Object.keys(filterDto).length){
+      return this.tasksService.getTasksWithFilter(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
 	}
 
 	@Get('/:id')
@@ -18,8 +24,23 @@ export class TasksController {
 		return this.tasksService.getTaskById(id);
 	}
 
-	@Post()
+  @Post()
+  @UsePipes(ValidationPipe)
 	createTask(@Body() createTaskDto: CreateTaskDto): Task {
 		return this.tasksService.createTask(createTaskDto);
 	}
+
+	@Delete('/:id')
+	deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string, 
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,10 +11,34 @@ export class TasksService {
 
 	public getAllTasks(): Task[] {
 		return this.tasks;
-	}
+  }
+  
+  public getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if(status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if(search) {
+      tasks = tasks.filter(task => 
+        task.title.includes(search) ||
+        task.description.includes(search),
+      );
+    }
+
+    return tasks;
+  }
 
 	public getTaskById(id: string): Task {
-		return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+    
+    if(!found){
+        throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
 	}
 
 	public createTask(createTaskDto: CreateTaskDto): Task {
@@ -27,5 +52,16 @@ export class TasksService {
 
 		this.tasks.push(task);
 		return task;
-	}
+  }
+  
+  public deleteTask(id: string): void {
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
+  }
+
+  public updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
 }
