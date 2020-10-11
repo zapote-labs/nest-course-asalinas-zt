ed72601ca86a91e755d211992b6e2fff93733820
asalinas-zt <adrianasalinas@zapote.org>
2020-10-11 15:31:23 -0500

    refactor(Tasks): Getting a task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
-	constructor(private tasksService: TasksService){}
+  constructor(private tasksService: TasksService){}
+  
+  @Get('/:id')
+	getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+		return this.tasksService.getTaskById(id);
+	}
 
 	/* @Get()
 	getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
@@ -16,11 +22,6 @@ export class TasksController {
     } else {
       return this.tasksService.getAllTasks();
     }
-	}
-
-	@Get('/:id')
-	getTaskById(@Param('id') id: string): Task {
-		return this.tasksService.getTaskById(id);
 	}
 
   @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,27 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TasksService {
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository,
+  ){}
 
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
+
+    if(!found){
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
+  }
 	/* public getAllTasks(): Task[] {
 		return this.tasks;
   }
@@ -27,16 +43,6 @@ export class TasksService {
 
     return tasks;
   }
-
-	public getTaskById(id: string): Task {
-    const found = this.tasks.find(task => task.id === id);
-    
-    if(!found){
-        throw new NotFoundException(`Task with ID "${id}" not found`);
-    }
-
-    return found;
-	}
 
 	public createTask(createTaskDto: CreateTaskDto): Task {
 		const { title, description } = createTaskDto;
