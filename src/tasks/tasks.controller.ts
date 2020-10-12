a0e0b28fce00514618ced9b18df7cddc9cbdc50b
asalinas-zt <adrianasalinas@zapote.org>
2020-10-12 09:36:41 -0500

    refactor(Tasks): Delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -20,6 +20,11 @@ export class TasksController {
 	createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
 		return this.tasksService.createTask(createTaskDto);
   }
+
+  @Delete('/:id')
+	deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
   
 	/* @Get()
 	getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
@@ -30,10 +35,7 @@ export class TasksController {
     }
 	}
 
-	@Delete('/:id')
-	deleteTask(@Param('id') id: string): void {
-    this.tasksService.deleteTask(id);
-  }
+	
 
   @Patch('/:id/status')
   updateTaskStatus(
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -28,6 +28,13 @@ export class TasksService {
     return this.taskRepository.createTask(createTaskDto);
   }
   
+  async deleteTask(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id);
+    
+    if(result.affected === 0){
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
 	/* public getAllTasks(): Task[] {
 		return this.tasks;
   }
@@ -48,11 +55,6 @@ export class TasksService {
     }
 
     return tasks;
-  }
-
-  public deleteTask(id: string): void {
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 
   public updateTaskStatus(id: string, status: TaskStatus): Task {
