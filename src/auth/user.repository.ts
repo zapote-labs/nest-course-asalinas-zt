2c9cc7430a15427d2323904c863f4577ae6a42bf
asalinas-zt <adrianasalinas@zapote.org>
2020-11-05 13:41:42 -0600

    feat(Authentication): Added validation of duplicate usernames

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,27 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { Repository, EntityRepository } from "typeorm";
 
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
+
 @EntityRepository(User)
 export class UserRepository extends Repository <User> {
 	async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
 		const { username, password } = authCredentialsDto;
+		
 		const user = new User();
 		user.username = username;
 		user.password = password;
-		await user.save();
+
+		try {
+			await user.save();
+		} catch(error) {
+			if(error.code === '23505') { //duplicate username
+				throw new ConflictException('Username already exists');
+			} else {
+				throw new InternalServerErrorException();
+			}
+		}
 	}
 }
