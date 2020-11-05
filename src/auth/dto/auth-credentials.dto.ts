59562c25cd2a1c4f32077f00d2bed1ffb84e0ea3
asalinas-zt <adrianasalinas@zapote.org>
2020-11-05 13:17:16 -0600

    feat(Authentication): Added validation of password strength

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, MaxLength, Min, MinLength, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+	@IsString()
+	@MaxLength(20)
+	@MinLength(4)
 	username: string;
+
+	@IsString()
+	@MaxLength(20)
+	@MinLength(8)
+	@Matches(
+		/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
+		{ message: 'password to weak'},
+	)
 	password: string;
 }
